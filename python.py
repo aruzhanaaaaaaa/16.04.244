086c1b8c4061925585730b21cd6e2bd4a7db5b42
aruzhanaaaaaaa <158404030+aruzhanaaaaaaa@users.noreply.github.com>
2024-04-17 11:52:38 +0500

    Update python.py

--- python.py
+++ python.py
@@ -1,69 +1,39 @@
-#for,while,dict,a=[];-массив,str
-
-a={
-    "car":"Toyoth",
-    "model":"camry",
-    "image":["https://www.toyotakz.com/?ysclid=lv0noozwl771248892"]
-}
+#функция,                                                  ооп
+#у=f(x)
+#def add(x,y):return(кайтару) x+y(xпараметр)              classA: def add (x,y):return x+y 
 
 
-b={
-    "car":"Toyoth",
-    "model":"camry",
-    "images":["https://www.toyotakz.com/?ysclid=lv0noozwl771248892,http://www.motorpage.ru/Toyota/photos.html?ysclid=lv0o41h0sq153109485"],
-    "price":25000000,
-    "is_published":True
-}
 
-c={"name":"Erbol","surname":"Askarov"}
-b=c["name"]
-b=c.get("name")
-b=None
-c["middle_name"]="Erzhanuly"
-print(c)
-#
-#person_data=dict
-#person_data["name"]="Askar"
-#last_name="Erlanov"
-#person_data["last_name"]=last_name
+def add(x,y):return x+y
+a=add(4,5)
+print(a)
 
 
-d={"name":"Askar","last_name":"Erlanov"}
-for k,v in d.items():
-    print("key",k)
-    print("value",v)
 
 
-list_1=[ {
+def add_to_list(x):
+    list1=[1,2,3,4]
+    list1.append(x)
+    return list1
+print(add_to_list(x))
+
+
+
+def add(x):
+    listt1=[ {
     "name":"Kanat",
     "last_name":"Erbolov",
-    "age":20
-},
-{
-   "name":"Askar",
-    "last_name":"Erzhanov",
-    "age":15
-},
-{
-    "name":"Kairat",
-    "last_name":"Zhandosov",
-    "age":45
-}
+    "Age":20},
+    {"name":"Askar",
+     "last_name":"Erzhanov",
+     "Age":15},
+    {"name":"Kairat",
+     "last_name":"Zhandosov",
+     "Age":45}
 ]
+    listt1.append(x)
+    return listt1
 
-list_1 = [
-    {"name": "Kanat", "last_name": "Erbolov", "age": 20},
-    {"name": "Askar", "last_name": "Erzhanov", "age": 15},
-    {"name": "Kairat", "last_name": "Zhandosov", "age": 45}
-]
-
-total_age = 0
-for person in list_1:
-    total_age += person["age"]
-    
-count = len(list_1)
-
-
-average_age = total_age / count
-
-print("Average age:", average_age)
+print(add({"name":"Aslan",
+           "last_name":"Askarovich",
+           "Age":"19"}))
